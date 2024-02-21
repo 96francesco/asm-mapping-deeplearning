ce9257fbc91b51b5b15b8b9efd72cf51b556127f
96francesco <francesco.pasanisi96@gmail.com>
2024-02-21 13:53:18 +0000

    edit setup to import custom modules

--- setup.py
+++ setup.py
@@ -2,9 +2,10 @@ from setuptools import find_packages, setup
 
 setup(
     name='src',
-    packages=find_packages(),
     version='0.1.0',
-    description='A short description of the project.',
+    description='Deep learning models for ASM mapping from satellite images.',
     author='Francesco Pasanisi',
     license='MIT',
+    packages=find_packages(where="src"),
+    package_dir={"": "src"}
 )
