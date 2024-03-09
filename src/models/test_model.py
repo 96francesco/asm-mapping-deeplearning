92d395aad5bdd5dd36d467df25fcf8f6fed535cb
96francesco <francesco.pasanisi96@gmail.com>
2024-03-09 13:28:31 +0000

    create config file for testing and adapt testing script

--- src/models/test_model.py
+++ src/models/test_model.py
@@ -1,36 +1,87 @@
 import pytorch_lightning as pl
+import segmentation_models_pytorch as smp
+import torch
+import json
 
+from pytorch_lightning.callbacks import EarlyStopping
+from pytorch_lightning.callbacks import ModelCheckpoint
 from pytorch_lightning import seed_everything
-from torch.utils.data import DataLoader
+from pytorch_lightning.strategies import DDPStrategy
+from torch.utils.data import DataLoader, random_split
 
 # import custom modules
-from models.lit_model_multiclass import LitModelMulticlass
-from data.normalization_functions import linear_norm_global_percentile
 from data.planet_dataset import PlanetDataset
+from data.s1_dataset import Sentinel1Dataset
+from data.fusion_dataset import FusionDataset
+
+from data.planet_dataset_normalization import linear_norm_global_percentile as planet_norm_percentile
+from data.planet_dataset_normalization import linear_norm_global_minmax as planet_norm_minmax
+from data.planet_dataset_normalization import global_standardization as planet_standardization
+
+from data.s1_dataset_normalization import global_standardization as s1_standardization
+from data.s1_dataset_normalization import linear_norm_global_minmax as s1_norm_minmax
+from data.s1_dataset_normalization import linear_norm_global_percentile as s1_norm_percentile
+
+from models.lit_model_binary import LitModelBinary
+from models.lit_model_multiclass import LitModelMulticlass
+from models.lit_model_fusion import LitModelBinaryLateFusion
 
 # set seed for reproducibility
 seed_everything(42, workers=True)
 
-# load testing set
-testing_dir = '/mnt/guanabana/raid/home/pasan001/thesis/dataset/asm_dataset_split_0/planet/multiclass/testing_data'
-normalization = linear_norm_global_percentile
-testing_dataset = PlanetDataset(testing_dir,
-                                pad=True,
-                                normalization=normalization)
+# read config file
+with open('src/models/test_config.json') as f:
+    config = json.load(f)
+
+mode_dict = {
+    "binary": LitModelBinary,
+    "multiclass": LitModelMulticlass,
+    "fusion": LitModelBinaryLateFusion
+}
+
+datasource_dict = {
+    "planet": PlanetDataset,
+    "s1": Sentinel1Dataset,
+    "fusion": FusionDataset
+}
+
+if datasource_dict[config["datasource"]] == PlanetDataset:
+    normalization_dict = {
+    "standardization": planet_standardization,
+    "percentile": planet_norm_percentile,
+    "minmax": planet_norm_minmax,
+}
+    dataset = PlanetDataset
+elif datasource_dict[config["datasource"]] == Sentinel1Dataset:
+    normalization_dict = {
+    "standardization": s1_standardization,
+    "minmax": s1_norm_minmax,
+    "percentile": s1_norm_percentile,
+}
+    dataset = Sentinel1Dataset
+
+# create testing dataset
+testing_dir = config["testing_dir"]
+normalization = normalization_dict[config["normalization"]]
+testing_dataset = dataset(testing_dir,
+                         pad=True,
+                         normalization=normalization)
 
 # load the checkpoint
-model = LitModelMulticlass.load_from_checkpoint(checkpoint_path='models/checkpoints/planet-multiclass-2-epoch=32-val_loss=0.02.ckpt')
+checkpoint = config["checkpoint"]
+model = LitModelMulticlass.load_from_checkpoint(checkpoint_path=checkpoint)
 
 # set the model for evaluation
 model.eval()
 model.freeze()
 
 # initialize dataloader
-batch_size = 16
-test_loader = DataLoader(testing_dataset, batch_size=batch_size, shuffle=False,
+batch_size = config["batch_size"]
+test_loader = DataLoader(testing_dataset, 
+                         batch_size=batch_size, 
+                         shuffle=False,
                          num_workers=4)
 
 # test the model
 trainer = pl.Trainer()
 trainer.test(model, test_loader)
-
