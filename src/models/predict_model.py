84951b1c7cbad7c3a102ee8d1e209ee1a392d6db
96francesco <francesco.pasanisi96@gmail.com>
2024-03-14 22:27:28 +0000

    reorganize predict model script

--- src/models/predict_model.py
+++ src/models/predict_model.py
@@ -1,43 +1,120 @@
-from torch.utils.data import DataLoader
+import pytorch_lightning as pl
+import torch
+import json
+import gc
+
 from pytorch_lightning import seed_everything
+from pytorch_lightning.strategies import DDPStrategy
+from torch.utils.data import DataLoader
 
 # import custom modules
+from data.planet_dataset import PlanetDataset
+from data.s1_dataset import Sentinel1Dataset
+from data.fusion_dataset import FusionDataset
+
+from data.planet_dataset_normalization import linear_norm_global_percentile as planet_norm_percentile
+from data.planet_dataset_normalization import linear_norm_global_minmax as planet_norm_minmax
+from data.planet_dataset_normalization import global_standardization as planet_standardization
+
+from data.s1_dataset_normalization import global_standardization as s1_standardization
+from data.s1_dataset_normalization import linear_norm_global_minmax as s1_norm_minmax
+from data.s1_dataset_normalization import linear_norm_global_percentile as s1_norm_percentile
+
 from models.lit_model_binary import LitModelBinary
 from models.lit_model_multiclass import LitModelMulticlass
+from models.lit_model_fusion import LitModelBinaryLateFusion
+
 from visualization.get_predictions import get_predictions
-from data.planet_dataset import PlanetDataset
-from data.normalization_functions import linear_norm_global_percentile
-from visualization.plot_binary_segmentation import plot_segmentation_outputs as plt_binary
-from visualization.plot_multiclass_segmentation import plot_segmentation_outputs as plt_multiclass
+from visualization.plot_binary_segmentation import plot_segmentation_outputs
+
 
 # set seed for reproducibility
 seed_everything(42, workers=True)
 
-# load testing set
-testing_dir = '/mnt/guanabana/raid/home/pasan001/thesis/dataset/asm_dataset_split_0/planet/multiclass/testing_data'
-normalization = linear_norm_global_percentile
-testing_dataset = PlanetDataset(testing_dir,
-                                pad=True,
-                                normalization=normalization)
+# clear CUDA cache
+torch.cuda.empty_cache()
+gc.collect()
 
-# initialize dataloader
-batch_size = 16
-test_loader = DataLoader(testing_dataset, batch_size=batch_size, shuffle=False,
-                         num_workers=4)
+# read config file
+with open('src/models/test_config.json') as f:
+    config = json.load(f)
+
+mode_dict = {
+    "binary": LitModelBinary,
+    "multiclass": LitModelMulticlass,
+    "fusion": LitModelBinaryLateFusion
+}
+
+datasource_dict = {
+    "planet": PlanetDataset,
+    "s1": Sentinel1Dataset,
+    "fusion": FusionDataset
+}
+
+if datasource_dict[config["datasource"]] == PlanetDataset:
+    normalization_dict = {
+    "standardization": planet_standardization,
+    "percentile": planet_norm_percentile,
+    "minmax": planet_norm_minmax,
+}
+    dataset = PlanetDataset
+elif datasource_dict[config["datasource"]] == Sentinel1Dataset:
+    normalization_dict = {
+    "standardization": s1_standardization,
+    "minmax": s1_norm_minmax,
+    "percentile": s1_norm_percentile,
+}
+    dataset = Sentinel1Dataset
+elif datasource_dict[config["datasource"]] == FusionDataset:
+    normalization_dict = {
+    "planet_minmax": planet_norm_minmax,
+    "s1_standardization": s1_standardization,
+}
+    dataset = FusionDataset
+
+# create testing dataset
+testing_dir = config["testing_dir"]
+
+if datasource_dict[config["datasource"]] == FusionDataset:
+    planet_normalization = normalization_dict[config["planet_normalization"]]
+    s1_normalization = normalization_dict[config["s1_normalization"]]
+    testing_dataset = dataset(testing_dir,
+                            train=False,
+                            planet_normalization=planet_normalization,
+                            s1_normalization=s1_normalization)
+else:
+    normalization = normalization_dict[config["normalization"]]
+    testing_dataset = dataset(testing_dir,
+                            pad=True,
+                            normalization=normalization)
 
 # load the checkpoint
-# model = LitModelBinary.load_from_checkpoint(checkpoint_path='models/checkpoints/planet-singleclass-epoch=42-val_loss=0.03.ckpt')
-model = LitModelMulticlass.load_from_checkpoint(checkpoint_path='models/checkpoints/planet-multiclass-2-epoch=32-val_loss=0.02.ckpt')
+model = mode_dict[config["mode"]]
+model = model.load_from_checkpoint(checkpoint_path=config["checkpoint"])
 
 # set the model for evaluation
 model.eval()
 model.freeze()
 
-# get_predictions(model, test_loader, indices=[18, 69, 112], output_name='planet-binary-predictions')
-get_predictions(model, test_loader, indices=[13, 222, 100], output_name='planet-multiclass-2-predictions')
+batch_size = config["batch_size"]
+test_loader = DataLoader(testing_dataset, 
+                         batch_size=batch_size, 
+                         shuffle=False,
+                         num_workers=4)
 
-# predictions_file = 'models/predictions/planet-binary-predictions_[18, 69, 112].pth'
-# plot_segmentation_outputs = plt_binary(predictions_file, 'planet-binary-predictions')
 
-predictions_file = 'models/predictions/planet-multiclass-2-predictions_[13, 222, 100].pth'
-plot_segmentation_outputs = plt_multiclass(predictions_file, 'planet-multiclass-2-predictions')
+indices = [18, 69, 112]
+filename = config['checkpoint_name']
+print(filename)
+get_predictions(model, 
+                test_loader, 
+                is_fusion=True,
+                indices=indices, 
+                output_name=f'{filename}')
+
+predictions_file = f'models/predictions/{filename}-{indices}.pth'
+plot_segmentation_outputs = plot_segmentation_outputs(predictions_file, 
+                                                      f'{filename}_{indices}.png',
+                                                      is_fusion=True,
+                                                      is_optical=False,
+                                                      threshold=0.6)
