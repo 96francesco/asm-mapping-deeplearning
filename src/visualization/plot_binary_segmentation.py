36898ebf84cf22ff9839f2867e3ff8c16c7c9b90
96francesco <francesco.pasanisi96@gmail.com>
2024-03-15 20:02:26 +0000

    fix comments

--- src/visualization/plot_binary_segmentation.py
+++ src/visualization/plot_binary_segmentation.py
@@ -11,13 +11,12 @@ def plot_segmentation_outputs(predictions_file: str, output_name: str, is_optica
       
       subplot_cols = 4 if is_fusion else 3
       fig, axs = plt.subplots(num_examples, subplot_cols, figsize=(20, 5 * num_examples))
-      #     plt.subplots_adjust(left=None, bottom=None, right=None, top=None, wspace=0.1, hspace=0.1)
       
       for i in range(min(num_examples, len(predictions))):
             if is_fusion:
                   planet_input, s1_input, outputs, targets = predictions[i]
                   
-                  # Processing for Planet input
+                  # process Planet input
                   planet_img = planet_input[0].cpu().numpy().transpose(1, 2, 0)
                   planet_img = planet_img[:, :, [2, 1, 0]]  # Convert BGR to RGB
                   planet_img = (planet_img - planet_img.min()) / (planet_img.max() - planet_img.min())
@@ -25,7 +24,7 @@ def plot_segmentation_outputs(predictions_file: str, output_name: str, is_optica
 
                   planet_img = planet_img[:original_height, :original_width]
                   
-                  # Processing for S1 input
+                  # process S1 input
                   s1_img = s1_input[0][0].cpu().numpy()
                   s1_img = s1_img[:original_height, :original_width]
 
@@ -38,7 +37,7 @@ def plot_segmentation_outputs(predictions_file: str, output_name: str, is_optica
                   
                   input_img = inputs[0].cpu().numpy().transpose(1, 2, 0)
                   if is_optical:
-                        input_img = input_img[:, :, [2, 1, 0]]  # Convert BGR to RGB
+                        input_img = input_img[:, :, [2, 1, 0]]  # convert BGR to RGB
                         
                         input_img = (input_img - input_img.min()) / (input_img.max() - input_img.min())
                         input_img = (input_img * 255).astype(np.uint8)
@@ -50,16 +49,16 @@ def plot_segmentation_outputs(predictions_file: str, output_name: str, is_optica
                         
                   input_img = input_img[:original_height, :original_width]
             
-            # Common processing for outputs and targets
+            # process outputs and targets
             target_img = targets[0].cpu().numpy()
             probs = torch.sigmoid(outputs[0].cpu()).detach().numpy()
             output_img = (probs > threshold).squeeze().astype(np.uint8)
             
-            # Cropping to original dimensions
+            # crop to original dimensions
             output_img = output_img[:original_height, :original_width]
             target_img = target_img[:original_height, :original_width]
 
-            # Plotting model segmentation and ground truth
+            # plot model segmentation and ground truth
             axs[i, subplot_cols-2].imshow(output_img, cmap='gray')
             axs[i, subplot_cols-2].set_title("Model Segmentation", fontsize=20)
             axs[i, subplot_cols-1].imshow(target_img, cmap='gray')
