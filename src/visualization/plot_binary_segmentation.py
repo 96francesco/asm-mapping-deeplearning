b621a9004bc934c863021a6a73b756a57e46c4ce
96francesco <francesco.pasanisi96@gmail.com>
2024-03-15 12:02:48 +0000

    add handling of fusion case

--- src/visualization/plot_binary_segmentation.py
+++ src/visualization/plot_binary_segmentation.py
@@ -2,58 +2,72 @@ import torch
 import numpy as np
 import matplotlib.pyplot as plt
 
-def plot_segmentation_outputs(predictions_file: str, output_name: str, is_optical=True, 
-                              num_examples=3, original_dimensions=(180, 180)):
-      # load saved predictions
+def plot_segmentation_outputs(predictions_file: str, output_name: str, is_optical=True,
+                               is_fusion=False, num_examples=3, original_dimensions=(375, 375),
+                               threshold=0.5):
+    
       predictions = torch.load(predictions_file)
-
-      fig, axs = plt.subplots(num_examples, 3, figsize=(15, 5 * num_examples))
-
+      original_height, original_width = original_dimensions
+      
+      subplot_cols = 4 if is_fusion else 3
+      fig, axs = plt.subplots(num_examples, subplot_cols, figsize=(20, 5 * num_examples))
+      #     plt.subplots_adjust(left=None, bottom=None, right=None, top=None, wspace=0.1, hspace=0.1)
+      
       for i in range(min(num_examples, len(predictions))):
-            inputs, outputs, targets = predictions[i]
+            if is_fusion:
+                  planet_input, s1_input, outputs, targets = predictions[i]
+                  
+                  # Processing for Planet input
+                  planet_img = planet_input[0].cpu().numpy().transpose(1, 2, 0)
+                  planet_img = planet_img[:, :, [2, 1, 0]]  # Convert BGR to RGB
+                  planet_img = (planet_img - planet_img.min()) / (planet_img.max() - planet_img.min())
+                  planet_img = (planet_img * 255).astype(np.uint8)
 
-            if is_optical:
-                  # reorder the bands from B, G, R, NIR to R, G, B for RGB natural color visualization
+                  planet_img = planet_img[:original_height, :original_width]
+                  
+                  # Processing for S1 input
+                  s1_img = s1_input[0][0].cpu().numpy()
+                  s1_img = s1_img[:original_height, :original_width]
+
+                  axs[i, 0].imshow(planet_img)
+                  axs[i, 0].set_title("Planet-NICFI Input", fontsize=20)
+                  axs[i, 1].imshow(s1_img, cmap='gray')
+                  axs[i, 1].set_title("Sentinel-1 Input", fontsize=20)
+            else:
+                  inputs, outputs, targets = predictions[i]
+                  
                   input_img = inputs[0].cpu().numpy().transpose(1, 2, 0)
-                  input_img = input_img[:, :, [2, 1, 0]]
-
-                  # normalize for visualization purposes
-                  input_img = (input_img - input_img.min()) / (input_img.max() - input_img.min())
-                  input_img = (input_img * 255).astype(np.uint8)
-            else:
-                  input_img = inputs[0][0].cpu().numpy()
-
-            # convert target to numpy array
-            target_img = targets[0].cpu().numpy()
-
-            # apply sigmoid to the output logits to get probabilities
-            probs = torch.sigmoid(outputs[0].cpu()).detach().numpy()
-
-            # apply threshold to get binary mask
-            output_img = (probs > 0.5).squeeze().astype(np.uint8)
-
-            # crop the images to the original dimensions
-            original_height, original_width = original_dimensions
-            input_img = input_img[:original_height, :original_width]
-            target_img = target_img[:original_height, :original_width]
-            output_img = output_img[:original_height, :original_width]
+                  if is_optical:
+                        input_img = input_img[:, :, [2, 1, 0]]  # Convert BGR to RGB
+                        
+                        input_img = (input_img - input_img.min()) / (input_img.max() - input_img.min())
+                        input_img = (input_img * 255).astype(np.uint8)
+                        
+                        axs[i, 0].imshow(input_img, cmap=None if is_optical else 'gray')
+                        axs[i, 0].set_title("Input Image", fontsize=20)
+                  else:
+                        input_img = inputs[0][0].cpu().numpy()
+                        
+                  input_img = input_img[:original_height, :original_width]
             
-            # make plot
-            if is_optical:
-                  axs[i, 0].imshow(input_img)
-                  axs[i, 0].set_title("Input Image", fontsize=20)
-            else:
-                  axs[i, 0].imshow(input_img, cmap='gray')
-                  axs[i, 0].set_title("Input Image", fontsize=20)
+            # Common processing for outputs and targets
+            target_img = targets[0].cpu().numpy()
+            probs = torch.sigmoid(outputs[0].cpu()).detach().numpy()
+            output_img = (probs > threshold).squeeze().astype(np.uint8)
+            
+            # Cropping to original dimensions
+            output_img = output_img[:original_height, :original_width]
+            target_img = target_img[:original_height, :original_width]
 
-            axs[i, 1].imshow(output_img, cmap='gray')
-            axs[i, 1].set_title("Model Segmentation", fontsize=20)
-            axs[i, 2].imshow(target_img, cmap='gray')
-            axs[i, 2].set_title("Ground Truth", fontsize=20)
+            # Plotting model segmentation and ground truth
+            axs[i, subplot_cols-2].imshow(output_img, cmap='gray')
+            axs[i, subplot_cols-2].set_title("Model Segmentation", fontsize=20)
+            axs[i, subplot_cols-1].imshow(target_img, cmap='gray')
+            axs[i, subplot_cols-1].set_title("Ground Truth", fontsize=20)
 
             for ax in axs[i]:
                   ax.axis("off")
 
       plt.tight_layout()
-      plt.show()
       plt.savefig(f'reports/{output_name}.png')
+      plt.show()
