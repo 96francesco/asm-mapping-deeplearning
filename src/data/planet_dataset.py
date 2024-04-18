cf7898c49cd369c31df840e1527bfb1f5a2de063
96francesco <francesco.pasanisi96@gmail.com>
2024-04-18 15:41:36 +0000

    add inference mode handling and fix docstrings

--- src/data/planet_dataset.py
+++ src/data/planet_dataset.py
@@ -10,60 +10,62 @@ from PIL import Image
 class PlanetDataset(Dataset):
       """
       A dataset class for Planet satellite images, supporting operations like padding, 
-      normalization, and data augmentation.
+      normalization, and data augmentation, and can handle different operational 
+      modes such as training, inference, and data fusion.
 
       Attributes:
-        data_dir (str): Path to the directory with image and ground truth data.
-        pad (bool): Whether to pad images to a fixed size.
-        normalization (callable, optional): Function for normalizing image data.
-        transforms (callable, optional): Augmentation transforms to be applied to the images.
-        img_folder (str): Subdirectory within data_dir containing the images.
-        gt_folder (str): Subdirectory within data_dir containing the ground truth images.
-        dataset (list of tuples): List where each tuple contains paths to an image 
-            and its corresponding ground truth.
+            data_dir (str): Path to the directory with image and ground truth data.
+            pad (bool): Whether to pad images to a fixed size.
+            normalization (callable, optional): Function for normalizing image data.
+            transforms (callable, optional): Augmentation transforms to be applied to the images.
+            is_fusion (bool): Whether the dataset is used for fusion with Sentinel-1 data.
+            is_inference (bool): Whether the dataset is used for inference.
+            img_folder (str): Subdirectory within data_dir containing the images.
+            gt_folder (str): Subdirectory within data_dir containing the ground truth images.
+            dataset (list of tuples): List where each tuple contains paths to an image 
+                  and its corresponding ground truth.
 
       Methods:
-        ndvi(img): Calculates the Normalized Difference Vegetation Index (NDVI) for an image.
-        savi(img, L=0.5): Calculates the Soil-Adjusted Vegetation Index (SAVI) for an image.
-        ndwi(img): Calculates the Normalized Difference Water Index (NDWI) for an image.
-        __len__(): Returns the number of image-ground truth pairs in the dataset.
-        pad_img(img_tensor, pad_height, pad_width): Pads the image tensor to the specified dimensions.
-        __getitem__(idx): Retrieves the image-ground truth pair at the specified index, 
-            applying any specified transformations.
+            ndvi(img): Calculates the Normalized Difference Vegetation Index (NDVI) for an image.
+            savi(img, L=0.5): Calculates the Soil-Adjusted Vegetation Index (SAVI) for an image.
+            ndwi(img): Calculates the Normalized Difference Water Index (NDWI) for an image.
+            __len__(): Returns the number of image-ground truth pairs in the dataset.
+            pad_img(img_tensor, pad_height, pad_width): Pads the image tensor to the specified dimensions.
+            __getitem__(idx): Retrieves the image-ground truth pair at the specified index, 
+                  applying any specified transformations.
       """
-    
-      def __init__(self, data_dir: str, pad=False, normalization=None, transforms=None, is_fusion=False):
-            self.data_dir = data_dir
-            self.pad = pad
-            self.normalization = normalization
-            self.transforms = transforms
-            self.is_fusion = is_fusion
+      def __init__(self, data_dir, pad=False, normalization=None, transforms=None, 
+                 is_fusion=False, is_inference=False):
+        self.data_dir = data_dir
+        self.pad = pad
+        self.normalization = normalization
+        self.transforms = transforms
+        self.is_fusion = is_fusion
+        self.is_inference = is_inference
 
-            if is_fusion:
-                  self.img_folder = os.path.join(data_dir)   
-                  goal_dir = os.path.join(data_dir,  '../../gt') 
-                  self.gt_folder = os.path.normpath(goal_dir)
-            else:
-                  self.img_folder = os.path.join(data_dir, 'images')
-                  self.gt_folder = os.path.join(data_dir, 'gt')
+        # set the correct image folder path based on the mode
+        if is_inference:
+            self.img_folder = os.path.join(data_dir) 
+        else:
+            self.img_folder = os.path.join(data_dir, 'images/planet') if is_fusion else os.path.join(data_dir, 'images')
 
-            # Get a sorted list of image and ground truth file names
-            img_filenames = sorted(os.listdir(self.img_folder))
+        self.gt_folder = os.path.join(data_dir, 'gt') if not is_inference else None
+
+        self.dataset = []
+        img_filenames = sorted(os.listdir(self.img_folder))
+
+        if not is_inference:
             gt_filenames = sorted(os.listdir(self.gt_folder))
-
-            img_prefix = 'nicfi_'
-            gt_prefix = 'nicfi_gt_'
-
-            # pair each input image with its corresponding ground truth
-            self.dataset = []
             for img_name in img_filenames:
-                  if img_name.startswith(img_prefix):
-                        # construct the corresponding ground truth file name
-                        gt_name = img_name.replace(img_prefix, gt_prefix)
-                  if gt_name in gt_filenames:
-                        img_path = os.path.join(self.img_folder, img_name)
-                        gt_path = os.path.join(self.gt_folder, gt_name)
-                        self.dataset.append((img_path, gt_path))
+                gt_name = 'nicfi_gt_' + img_name.split('_')[-1]  # Assuming naming convention for GT files
+                if gt_name in gt_filenames:
+                    img_path = os.path.join(self.img_folder, img_name)
+                    gt_path = os.path.join(self.gt_folder, gt_name)
+                    self.dataset.append((img_path, gt_path))
+        else:
+            for img_name in img_filenames:
+                img_path = os.path.join(self.img_folder, img_name)
+                self.dataset.append(img_path)
 
       @staticmethod
       def ndvi(img):
@@ -115,7 +117,11 @@ class PlanetDataset(Dataset):
 
 
       def __getitem__(self, idx):
-            img_path, gt_path = self.dataset[idx]
+            if self.is_inference:
+                  img_path = self.dataset[idx]  # no gt_path expected in inference mode
+                  gt_path = None  # set gt_path to None for clarity
+            else:
+                  img_path, gt_path = self.dataset[idx]
 
             # read image
             # conversiong to float32 is done to ensure correct normalization
@@ -138,22 +144,37 @@ class PlanetDataset(Dataset):
             # convert input image to pytorch tensor
             img_tensor = torch.from_numpy(img).float()
 
-            # Stack NDVI as an additional channel or use it as needed
+            # stack NDVI as an additional channel or use it as needed
             img_tensor = torch.cat((img_tensor, ndvi, savi, ndwi), dim=0)
 
-            # read ground truth and convert to tensor
-            gt = np.array(Image.open(gt_path).convert('L'), dtype=np.float32)
-            gt_tensor = torch.from_numpy(gt).long() # cross-entropy expects Long
+            
+            if not self.is_inference:
+                  gt = np.array(Image.open(gt_path).convert('L'), dtype=np.float32)
+                  gt_tensor = torch.from_numpy(gt).long()
+                  
+                  if self.pad:
+                        target_height = 384  #
+                        target_width = 384
 
-            if self.pad is True:
-                  target_height = 384  # Adjust
-                  target_width = 384
+                        pad_height = (target_height - img_tensor.shape[1] % target_height) % target_height
+                        pad_width = (target_width - img_tensor.shape[2] % target_width) % target_width
 
-                  pad_height = (target_height - img_tensor.shape[1] % target_height) % target_height
-                  pad_width = (target_width - img_tensor.shape[2] % target_width) % target_width
+                        img_tensor = self.pad_img(img_tensor, pad_height, pad_width)
+                        gt_tensor = self.pad_img(gt_tensor, pad_height, pad_width)
+                        gt_tensor = gt_tensor.long()  # convert back to Long type
+                  
+                  return img_tensor, gt_tensor
 
-                  img_tensor = self.pad_img(img_tensor, pad_height, pad_width)
-                  gt_tensor = self.pad_img(gt_tensor, pad_height, pad_width)
-                  gt_tensor = gt_tensor.long() # convert back to Long type
+            # if in inference mode, return only image tensor
+            else:
+                  # apply padding
+                  if self.pad:
+                        target_height = 384 
+                        target_width = 384
 
-            return img_tensor, gt_tensor
+                        pad_height = (target_height - img_tensor.shape[1] % target_height) % target_height
+                        pad_width = (target_width - img_tensor.shape[2] % target_width) % target_width
+
+                        img_tensor = self.pad_img(img_tensor, pad_height, pad_width)
+
+                  return img_tensor
