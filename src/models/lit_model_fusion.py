d6435b7f6be43d1e65479476f05936da29374b10
96francesco <francesco.pasanisi96@gmail.com>
2024-04-23 15:51:31 +0000

    remove redundant lines and add docstrings

--- src/models/lit_model_fusion.py
+++ src/models/lit_model_fusion.py
@@ -5,6 +5,35 @@ import segmentation_models_pytorch as smp
 import torchmetrics
 
 class LitModelLateFusion(pl.LightningModule):
+      """
+      A PyTorch Lightning module for late fusion of models trained on different data streams.
+      Specifically designed for fusing models trained separately on Sentinel-1 and Planet satellite
+      image data. Supports various loss functions and optimizers for training the fused model.
+
+      Attributes:
+            s1_model (torch.nn.Module): stream for processing Sentinel-1 satellite images.
+            planet_model (torch.nn.Module): stream for processing Planet satellite images.
+            fusion_conv (torch.nn.Conv2d): Convolutional layer to fuse features from s1_model and planet_model.
+            s1_loss (torch.nn.modules.loss._Loss): Loss function used for training the s1_model.
+            planet_loss (torch.nn.modules.loss._Loss): Loss function used for training the planet_model.
+            fusion_criterion (torch.nn.modules.loss._Loss): Loss function used for the fused output.
+            accuracy (torchmetrics.Accuracy): Metric for calculating the accuracy of predictions.
+            precision (torchmetrics.Precision): Metric for calculating the precision of predictions.
+            recall (torchmetrics.Recall): Metric for calculating the recall of predictions.
+            f1_score (torchmetrics.F1Score): Metric for calculating the F1 score of predictions.
+            threshold (float): Threshold value for converting logits to binary outputs.
+            optimizer (torch.optim.Optimizer): Optimizer used for training the model.
+      
+      Methods:
+            forward(planet_input, s1_input): Defines the forward pass of the module by fusing features from
+                  both input streams and applying the fusion_conv layer.
+            training_step(batch, batch_idx): Processes a single batch during training.
+            validation_step(batch, batch_idx): Processes a single batch during validation.
+            test_step(batch, batch_idx): Processes a single batch during testing, calculates and logs metrics.
+            configure_optimizers(): Configures and returns the model's optimizers and learning rate schedulers.
+            load_model_from_checkpoint(checkpoint_path, in_channels): Utility method for loading a model
+                  from a specified checkpoint.
+      """
       def __init__(self, planet_checkpoint=None, s1_checkpoint=None,
                   fusion_loss='ce', lr=1e-3, threshold=0.5, optimizer='adam',
                   s1_in_channels=2, planet_in_channels=7, weight_decay=1e-5, pos_weight=None):
@@ -17,10 +46,6 @@ class LitModelLateFusion(pl.LightningModule):
                   # load Sentinel-1 model from checkpoint
                   self.s1_model = self.load_model_from_checkpoint(s1_checkpoint, 
                                                                   self.hparams.s1_in_channels)
-                  # for param in self.s1_model.parameters():
-                  #       param.requires_grad = False
-
-
             else:
                   # initialize a new Sentinel-1 model
                   self.s1_model = smp.Unet(
@@ -87,9 +112,6 @@ class LitModelLateFusion(pl.LightningModule):
             self.f1_score = torchmetrics.F1Score(task='binary', 
                                                 average='macro',
                                                 threshold=self.threshold)
-            self.iou = torchmetrics.JaccardIndex(task='binary',
-                                                average='macro',
-                                                threshold=self.threshold)
 
       def forward(self, planet_input, s1_input):
             s1_features = self.s1_model(s1_input)
@@ -101,14 +123,11 @@ class LitModelLateFusion(pl.LightningModule):
       def training_step(self, batch, batch_idx):
             planet_input, s1_input, labels = batch
             labels = labels.unsqueeze(1).type_as(s1_input)
-
-            s1_output = self.s1_model(s1_input)
-            planet_output = self.planet_model(planet_input)
             fused_output = self.forward(planet_input, s1_input)
-
             loss = self.fusion_criterion(fused_output, labels)
 
-            self.log('train_loss', loss, on_step=False, on_epoch=True, prog_bar=True, sync_dist=True)
+            self.log('train_loss', loss, on_step=False, on_epoch=True, 
+                     prog_bar=True, sync_dist=True)
             return loss
 
       def validation_step(self, batch, batch_idx):
@@ -137,16 +156,17 @@ class LitModelLateFusion(pl.LightningModule):
             self.log('test_precision', self.precision(preds, labels), sync_dist=True)
             self.log('test_recall', self.recall(preds, labels), sync_dist=True)
             self.log('test_f1score', self.f1_score(preds, labels), sync_dist=True)
-            self.log('test_iou', self.iou(preds, labels), sync_dist=True)
 
             return
 
       def configure_optimizers(self):
                   params = list(self.s1_model.parameters()) + list(self.planet_model.parameters()) + list(self.fusion_conv.parameters())
                   if self.hparams.optimizer == 'adam':
-                        optimizer = torch.optim.Adam(params, lr=self.hparams.lr, weight_decay=self.hparams.weight_decay)
+                        optimizer = torch.optim.Adam(params, lr=self.hparams.lr, 
+                                                     weight_decay=self.hparams.weight_decay)
                   elif self.hparams.optimizer == 'sgd':
-                        optimizer = torch.optim.SGD(params, lr=self.hparams.lr, weight_decay=self.hparams.weight_decay)
+                        optimizer = torch.optim.SGD(params, lr=self.hparams.lr, 
+                                                    weight_decay=self.hparams.weight_decay)
                   else:
                         raise ValueError(f'Unknown optimizer: {self.hparams.optimizer}')
                   
