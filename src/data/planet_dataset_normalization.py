94e4297485d2714523b37731db01a28c994e5097
96francesco <francesco.pasanisi96@gmail.com>
2024-05-12 11:47:31 +0000

    update global statistics for normalization

--- src/data/planet_dataset_normalization.py
+++ src/data/planet_dataset_normalization.py
@@ -1,19 +1,19 @@
 import numpy as np
 
-def linear_norm_global_percentile(image, min_val=0, max_val=3481.5):
+def linear_norm_global_percentile(image, min_val=206.5, max_val=3770.5):
     """
     Normalize the image linearly using global 2nd and 98th percentiles as 
     min and max values.
     """
     return (image - min_val) / (max_val - min_val)
 
-def linear_norm_global_minmax(image, min_val=0, max_val=6098.5):
+def linear_norm_global_minmax(image, min_val=0, max_val=6210.5):
     """
     Normalize the image linearly using global min and max values.
     """
     return (image - min_val) / (max_val - min_val)
 
-def global_standardization(image, global_mean=573.88, global_std=981.79):
+def global_standardization(image, global_mean=1051.1990928820467, global_std=1133.3946195598342):
     """
     Normalize the image using global mean and standard deviation in order to have
     a mean of 0 and a standard deviation of 1.
