f41c9fd7b051a96d13293bd6b64e75ee804d47b7
96francesco <francesco.pasanisi96@gmail.com>
2024-06-14 09:34:36 +0000

    set correct formati

--- src/utils/download_gee_asset.py
+++ src/utils/download_gee_asset.py
@@ -2,7 +2,9 @@ import ee
 import requests
 
 def download_gee_asset(asset_id, ee_object, local_path):
-      url = ee.Image(asset_id).getDownloadURL({'scale': 4.77, 'region': ee_object.geometry()})
+      url = ee.Image(asset_id).getDownloadURL({'scale': 4.77, 
+                                               'region': ee_object.geometry(),
+                                               'format': 'GEO_TIFF'})
       response = requests.get(url)
       with open(local_path, 'wb') as f:
             f.write(response.content)
