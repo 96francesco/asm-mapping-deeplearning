fix image normalization

src/data/planet_dataset.py
@@ -18,7 +18,7 @@ class PlanetDataset(Dataset):
       Attributes:
             data_dir (str): Path to the directory with image and ground truth data.
             pad (bool): Whether to pad images to a fixed size.
-            normalization (bool): Specifies if normalization should be applied to the images.
+            normalization (callable): Calls a normalization function on the image tensor.
             transforms (callable, optional): Augmentation transforms to be applied to the images.
             is_fusion (bool): Whether the dataset is used for fusion with Sentinel-1 data.
             is_inference (bool): Whether the dataset is used for inference.
@@ -102,15 +102,6 @@ class PlanetDataset(Dataset):
 
             return torch.from_numpy(ndwi).unsqueeze(0)
       
-      @staticmethod
-      def normalize_percentile(image, 
-                            lower_percentile=206.5,
-                            upper_percentile=3770.5):
-        """
-        Normalize iamge between the global 2nd and 98th percentiles.
-        These values were previously calculated from the training dataset.
-        """
-        return (image - lower_percentile) / (upper_percentile - lower_percentile)
 
       def __len__(self):
             return len(self.dataset)
@@ -160,7 +151,7 @@ class PlanetDataset(Dataset):
 
             # apply normalization
             if self.normalization:
-                  img_tensor = self.normalize_percentile(img_tensor)
+                  img_tensor = self.normalization(img_tensor)
 
             # stack NDVI as an additional channel or use it as needed
             img_tensor = torch.cat((img_tensor, ndvi, savi, ndwi), dim=0)
@@ -198,8 +189,8 @@ class PlanetDataset(Dataset):
             else:
                   # apply padding
                   if self.pad:
-                        target_height = 1024 # inference is executd on larger tiles
-                        target_width = 1024
+                        target_height = 384
+                        target_width = 384
 
                         pad_height = (target_height - img_tensor.shape[1] % target_height) % target_height
                         pad_width = (target_width - img_tensor.shape[2] % target_width) % target_width

src/data/s1_dataset.py
@@ -21,7 +21,7 @@ class Sentinel1Dataset(Dataset):
     Attributes:
         data_dir (str): Path to the directory containing Sentinel-1 image and ground truth data.
         pad (bool): Indicates whether to pad images to a fixed size.
-        normalization (bool): Specifies if normalization should be applied to the images.
+        normalization (callable): Calls image normalization function.
         transforms (bool): Specifies if data augmentation should be applied to the images.
         is_fusion (bool): Specifies if the dataset is being used in fusion with Planet images,
                           enabling resampling to match Planet images' resolution.
@@ -109,10 +109,6 @@ class Sentinel1Dataset(Dataset):
             # match Planet images dimensions
             target_height = 384
             target_width = 384
-        
-        if self.is_inference:
-            target_height = 1024 # inference is executed on larger tiles
-            target_width = 1024
 
         # calculate padding
         pad_height = target_height - height if height < target_height else 0
@@ -164,7 +160,8 @@ class Sentinel1Dataset(Dataset):
 
         # normalize the image
         if self.normalization:
-            img = self.normalize_percentile(img)
+            # img = self.normalize_percentile(img)
+            img = self.normalization(img)
         
         # convert image to tensor
         img_tensor = torch.from_numpy(img).float()